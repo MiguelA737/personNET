47fc394ef94bb8da388bd492c3a37102e60ef61c
miih737 <miguel.aguena737@gmail.com>
2019-10-22 08:45:04 -0300

    Autenticação perfeitamente funcional!!!!!!!!!!!!!!!!!!! <thumbs-up><thumbs-up><thumbs-up><thumbs-up><thumbs-up>
    Signed-off-by: miih737 <miguel.aguena737@gmail.com>

---
+++ MyWebApp/Controllers/UserSignupController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyWebApp.Models;
+using MyWebApp.Repositories.Authentication;
+
+namespace MyWebApp.Controllers
+{
+    public class UserSignupController : Controller
+    {
+        private personNETEntities db = new personNETEntities();
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdUser,Name,E_mail,Pass,DirProfilePhoto,BirthDate,LastAccess,HasPremiumAccess")] TB_User tB_User)
+        {
+            if (db.TB_User.ToList().Find(x => x.E_mail.Equals("" + tB_User.E_mail.GetHashCode())) == null)
+            {
+                if (ModelState.IsValid)
+                {
+                    tB_User.Pass = "" + tB_User.Pass.GetHashCode();
+                    tB_User.E_mail = "" + tB_User.E_mail.GetHashCode();
+                    db.TB_User.Add(tB_User);
+                    db.SaveChanges();
+                    return RedirectPermanent("../Home/Index");
+                }
+            }
+            return View(tB_User);
+        }
+
+    }
+}
